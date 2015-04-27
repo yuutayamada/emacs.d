adf62f026e57112bd8964f18e928737a6ff77e4c
Yuta Yamada <cokesboy@gmail.com>
2015-04-26 23:40:41 -0700

    Update markdown-mode config

--- .emacs/pkg_conf/init_markdown-mode.el
+++ .emacs/pkg_conf/init_markdown-mode.el
@@ -1,15 +1,14 @@
 ;;; init_markdown-mode.el --- init file for markdown-mode
 ;;; Commentary:
 ;;; Code:
-
 (require 'markdown-mode)
-(require 'my_autoload)
+
 ;; http://stackoverflow.com/questions/7694887/is-there-a-command-line-utility-for-rendering-github-flavored-markdown
 (add-hook 'markdown-mode-hook
-          #'(lambda ()
-              (auto-complete-mode t)
-              (mykie:set-keys markdown-mode-map
-                "M-n" "M-p")))
+          '(lambda ()
+             (auto-complete-mode t)
+             (mykie:set-keys markdown-mode-map
+               "M-[" "M-n" "M-p")))
 
 (provide 'init_markdown-mode)
 

05a5a61d9e69f9a802c5566232ba931d31c75fbd
Yuta Yamada <cokesboy@gmail.com>
2015-05-02 14:43:48 -0700

    Add memo script

---
+++ install/arduino.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Arduino version 1.5.1 allow you to command line manipulation
+# http://www.arduino.cc/en/Main/Software
+
+# add dialout group to use arduino without sudo
+sudo addgroup $USERNAME dialout
+
+
+
+# old info
+# https://github.com/sudar/Arduino-Makefile/
+# Install Requirements for Arduino development
+# sudo apt-get install arduino arduino-mk python-serial

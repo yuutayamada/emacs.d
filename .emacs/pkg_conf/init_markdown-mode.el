b563f80a390e7e82391dfad7b080ed323f4a9e77
Yuta Yamada <cokesboy@gmail.com>
2015-07-29 16:33:05 -0700

    Add preview function by using grip

--- .emacs/pkg_conf/init_markdown-mode.el
+++ .emacs/pkg_conf/init_markdown-mode.el
@@ -12,6 +12,26 @@
                      (and dir (not (equal "~/" dir))))
                (gfm-mode))))
 
+;; memo: install grip > ‘pip3 install grip‘
+(defvar Y/markdown-process nil)
+(defun Y/markdown-preview (&rest _)
+  "Preview markdown file by using grip."
+  (let ((gfm (if (eq major-mode 'gfm-mode) "--gfm" "")))
+    (when (process-live-p Y/markdown-process)
+      (kill-process Y/markdown-process))
+    (setq Y/markdown-process
+          (start-process-shell-command "emacs-markdown-preview"
+                                       markdown-output-buffer-name
+                                       (format "grip --browser %s %s" gfm buffer-file-name)))
+    (run-with-timer 10 ; sec
+                    nil (lambda ()
+                          (when (process-live-p Y/markdown-process)
+                            (kill-process Y/markdown-process))))))
+
+;; Use advice to use preview at both markdown-mode and gfm-mode.
+(when (executable-find "grip")
+  (advice-add 'markdown-preview :override 'Y/markdown-preview))
+
 (provide 'init_markdown-mode)
 
 ;; Local Variables:

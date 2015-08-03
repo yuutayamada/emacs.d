b42e88331b61a05f966ea8b7c49a5be0e281b89e
Yuta Yamada <cokesboy@gmail.com>
2015-08-02 20:28:03 -0700

    Apply GUI emacs

--- .emacs/pkg_conf/init_avy.el
+++ .emacs/pkg_conf/init_avy.el
@@ -7,8 +7,13 @@
 
 ;;;###autoload
 (defun avy-goto-char-by-input-event ()
+  "Goto char with avy and move to ‘last-input-event’s char."
   (interactive)
-  (avy-goto-char last-input-event))
+  (if (characterp last-input-event)
+      (avy-goto-char last-input-event) ; using xterm-keybinder
+    ;; For GUI Emacs
+    (let ((keyseq (string-to-list (key-description (vector last-input-event)))))
+      (avy-goto-char (car (last keyseq))))))
 
 (provide 'init_avy)
 

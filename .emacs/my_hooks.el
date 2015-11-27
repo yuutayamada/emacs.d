bdc5a7c0041597119e9c79f637fa0caebc6df44e
Yuta Yamada <cokesboy@gmail.com>
2015-11-27 15:47:22 -0800

    Add require statement

--- .emacs/my_hooks.el
+++ .emacs/my_hooks.el
@@ -66,10 +66,12 @@
              ;; (git-gutter:linum-setup) ; for linum
              ))
 
-;; Use Evil's normal mode in actual file.
+;; Use Evil's normal mode in actual files.
 (add-hook 'find-file-hook
           '(lambda () ; don't activete normal state at magit's git commit buffer
-             (unless (bound-and-true-p with-editor-mode) (evil-normal-state))))
+             (unless (bound-and-true-p with-editor-mode)
+               (require 'evil)
+               (evil-emacs-state))))
 
 ;; auto-insert mode
 ;; https://www.gnu.org/software/emacs/manual/html_node/autotype/Autoinserting.html

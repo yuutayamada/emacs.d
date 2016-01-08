dacc97393095c542f56f90023ab827aecf595fde
Yuta Yamada <cokesboy@gmail.com>
2016-01-08 01:22:48 -0800

    Update nim-mode

--- .emacs/pkg_conf/init_nim-mode.el
+++ .emacs/pkg_conf/init_nim-mode.el
@@ -6,47 +6,31 @@
 (require 'nim-mode)
 (require 'company-nim)
 
-(setq nim-use-smie-indent t)
-
-;; Indent accordingly by the length of statements.
-(defconst nim-uncompleted-condition-indent 'stmt+1)
-
-(add-hook 'nim-mode-hook
-          '(lambda () (add-to-list 'company-backends 'company-nim)))
-
-;; need to build nimsuggest and move nimsuggest bin file to Nim's bin directory.
-(defconst nim-nimsuggest-path
-  (format "%s/github.com/nim-lang/Nim/bin/nimsuggest"
-          (shell-command-to-string "echo -n `ghq root`")))
-
-;; (defadvice nim-indent-line (around prevent-indent activate)
-;;   "Use previous indent level."
-;;   (when (and (not noninteractive) (looking-back (rx line-start (0+ space))))
-;;     (ad-set-arg 0 t))
-;;   ad-do-it)
-
-(defun Y/nim-smart-delete-backward-char ()
+(defun nim-electric-backward-char ()
   "If current line is just an empty line, indent backward.
 Otherwise, work as ‘backward-delete-char‘."
   (interactive)
-  (if (looking-back (rx line-start (1+ " ")) nil)
+  (if (and (looking-back (rx line-start (1+ " ")) nil)
+           (not (nth 3 (syntax-ppss)))
+           (not (nth 4 (syntax-ppss))))
       (nim-indent-line t)
     (backward-delete-char 1)))
 
-(define-key nim-mode-map (kbd "C-h") 'Y/nim-smart-delete-backward-char)
+(add-hook 'nim-mode-hook
+          '(lambda ()
+             (define-key nim-mode-map (kbd "C-h")
+               'nim-electric-backward-char)
+             (define-key nim-mode-map (kbd "TAB")
+               (lambda ()
+                 (interactive)
+                 (setq this-command 'indent-for-tab-command)
+                 (mykie:global-map:<tab>:key)))
+             (add-to-list 'company-backends 'company-nim)))
 
-(defadvice nim-mode-smie-rules (around output-debug-message activate)
-  "Output debug information."
-  (let ((k (ad-get-arg 0))
-        (v (ad-get-arg 1))
-        (fmt (concat "kind(%s)-Token(%s)-Point(%d)\n"
-                     "sibling(%s)-parent(%s)-hanging(%s==%s)")))
-    ad-do-it
-    (message (format fmt k v (point)
-                     (ignore-errors (smie-rule-sibling-p))
-                     (ignore-errors smie--parent)
-                     (ignore-errors (smie-rule-hanging-p))
-                     ad-return-value))))
+;; need to build nimsuggest and move nimsuggest bin file to Nim's bin directory.
+(defconst nim-nimsuggest-path
+  (format "%s/github.com/nim-lang/nimsuggest/nimsuggest"
+          (shell-command-to-string "echo -n `ghq root`")))
 
 (provide 'init_nim-mode)
 

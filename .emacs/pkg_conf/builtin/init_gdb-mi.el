42d9afe44538bdee550209015de46876033d8ce9
Yuta Yamada <cokesboy@gmail.com>
2017-09-03 20:15:43 -0700

    Add gdb config

---
+++ .emacs/pkg_conf/builtin/init_gdb-mi.el
@@ -0,0 +1,16 @@
+;;; init_gdb-mi.el ---                               -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'gdb-mi)
+
+;; show many buffers for helpful info of GDB
+(setq gdb-many-windows t)
+
+;; `gdb-restore-windows' - To restore the window layout.
+;; GDBHISTFILE
+;;
+(provide 'init_gdb-mi)
+;;; init_gdb-mi.el ends here
